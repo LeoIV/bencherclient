91e428742b138c250c5f91a6190581e5b8fd8db7
Leonard Papenmeier <leonard.papenmeier@gmail.com>
2024-02-15 10:40:53 +0100

    Add request library to dev dependencies
    The 'requests' library has been added to the group of dev_dependencies in pyproject.toml, increasing its version to "^2.31.0". This change subsequently led to several other dependencies being added

---
+++ run_all_benchmarks.py
@@ -0,0 +1,39 @@
+import grpc
+import requests
+from bencherscaffold.bencher_pb2 import BenchmarkRequest
+from bencherscaffold.bencher_pb2_grpc import BencherStub
+
+if __name__ == '__main__':
+    stub = BencherStub(
+        grpc.insecure_channel(f"127.0.0.1:{50051}")
+    )
+
+    headers = {
+        'Authorization': 'token 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',
+        'Accept'       : 'application/vnd.github.v3.raw',
+    }
+
+    response = requests.get(
+        'https://raw.githubusercontent.com/LeoIV/bencher/master/BencherServer/bencherserver/benchmark-registry.json',
+        headers=headers
+    )
+
+    registry = response.json()
+
+    for benchmarkname, properties in registry.items():
+
+        dimensions = properties['dimensions']
+
+        print(f"benchmarkname: {benchmarkname}, dimensions: {dimensions}")
+
+        try:
+            res = stub.EvaluatePoint(
+                BenchmarkRequest(
+                    benchmark=benchmarkname,
+                    point={'values': [1] * dimensions}
+                )
+            )
+            print(res)
+        except grpc.RpcError as e:
+            print(e.details())
+            continue
